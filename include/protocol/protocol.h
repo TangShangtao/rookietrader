8e20dc15dbb2fc3d2d72a34df01f7ccb2d9f57d1
tst <2643659274@qq.com>
2024-04-03 17:49:10 +0800

    first commit

---
+++ include/protocol/protocol.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "enum.h"
+#include "struct.h"
+#include "type.h"
---
+++ include/protocol/struct.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "protocol.h"
+// Data
+struct Tick
+{
+    
+};
+
+struct Bar
+{
+    
+};
+
+struct Trade
+{
+    
+};
+
+struct Order
+{
+
+};
+
+struct Position
+{
+
+};
+
+struct Account
+{
+
+};
+
+// Requests
+struct SubscribeReq
+{
+
+};
+
+struct OrderReq
+{
+    
+};
+
+struct CancelReq
+{
+
+};
+
+struct QryPositionReq
+{
+
+};
+
+struct QryAccountReq
+{
+
+};
+
