8cffa04c69103eecdfb6bc131206d302d70d9a50
tst <2643659274@qq.com>
2024-05-29 13:21:32 +0800

    test.py update

--- test.py
+++ test.py
@@ -5,19 +5,25 @@ import json
 class MDReceiver(TradeApi.MDApi):
     def __init__(self, eventUrl, rpcUrl, loggerName, logMode):
         super().__init__(eventUrl, rpcUrl, loggerName, logMode)
+        self.Subscribe(Protocol.EventType.EventMDReady)
+        # self.Subscribe(Protocol.EventType.EventTick)
         self.Init()
+        print("hello")
+
+    def OnMDApiStart(self):
+        print("python OnMDReady")
         self.SendPrepareMDReq()
 
     def OnMDReady(self, event):
         print("python OnMDReady")
-    
+        subInstruments = ["IH2406", "IH2407"]
+        self.SendSubTickReq(Protocol.ExchangeID.SHFE, subInstruments)
+
     def OnTick(self, event):
         print(f"python OnTick {event.instrumentID} {event.lastPrice}")
     
     def OnPrepareMDRsp(self, rsp):
         print("python OnPrepareMDRsp")
-        subInstruments = ["IH2406", "IH2407"]
-        self.SendSubTickReq(Protocol.ExchangeID.SHFE, subInstruments)
 
     def OnSubTickRsp(self, rsp):
         print("python OnSubTickRsp")
@@ -34,4 +40,3 @@ if __name__ == "__main__":
                           config["logMode"])
     while True:
         time.sleep(10)
-
